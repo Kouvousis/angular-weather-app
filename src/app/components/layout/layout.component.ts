0768cb0531e7ead84d6d19c1a59b710b3356bf37
Kouvousis <dexous1996@gmail.com>
2025-06-19 23:13:03 +0300

    fix: removed assets folder and moved background folder to public for the dynamic background to show properly

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component, Input, OnChanges } from '@angular/core';
 import { WeatherData } from '../../interfaces/weather-data';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, SearchBarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
@@ -16,27 +17,27 @@ export class LayoutComponent {
 
   get backgroundImageUrl(): string {
     if (!this.weatherData) {
-      return 'url(assets/backgrounds/default-background.jpg)';
+      return 'url(backgrounds/default-background.jpg)';
     }
     const weatherMain = this.weatherData.weather[0].main.toLowerCase();
 
     switch (weatherMain) {
       case 'atmosphere':
-        return 'url(assets/backgrounds/atmosphere.jpg)';
+        return 'url(backgrounds/atmosphere.jpg)';
       case 'clear':
-        return 'url(assets/backgrounds/clear.jpg)';
+        return 'url(backgrounds/clear.jpg)';
       case 'clouds':
-        return 'url(assets/backgrounds/clouds.jpg)';
+        return 'url(backgrounds/clouds.jpg)';
       case 'drizzle':
-        return 'url(assets/backgrounds/drizzle.jpg)';
+        return 'url(backgrounds/drizzle.jpg)';
       case 'rain':
-        return 'url(assets/backgrounds/rain.jpg)';
+        return 'url(backgrounds/rain.jpg)';
       case 'snow':
-        return 'url(assets/backgrounds/snow.jpg)';
+        return 'url(backgrounds/snow.jpg)';
       case 'thunderstorm':
-        return 'url(assets/backgrounds/thunderstorm.jpg)';
+        return 'url(backgrounds/thunderstorm.jpg)';
       default:
-        return 'url(assets/backgrounds/default-background.jpg)';
+        return 'url(backgrounds/default-background.jpg)';
     }
   }
 }
