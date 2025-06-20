f0ac922a840fdb11f8abbaeb28ed0a1127d31dd9
Kouvousis <dexous1996@gmail.com>
2025-06-20 21:18:21 +0300

    style: used lucide icon instead of svg

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,18 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { WeatherData } from '../../interfaces/weather-data';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule, Heart } from 'lucide-angular';
 
 @Component({
   selector: 'app-weather',
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
   @Input() weatherData!: WeatherData;
   @Input() favorites: string[] = [];
-
   @Output() toggleFavorite = new EventEmitter<string>();
+
+  readonly Heart = Heart;
 
   onToggleFavorite(): void {
     this.toggleFavorite.emit(this.weatherData.name);
